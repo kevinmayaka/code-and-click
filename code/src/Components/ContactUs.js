2f37f0edad793f1a89698a697351e8f9b30bfc9d
berlin <kevinabuga3@gmail.com>
2025-12-08 14:50:01 +0300

    adds form to collect client's data

--- code/src/Components/ContactUs.js
+++ code/src/Components/ContactUs.js
@@ -1,4 +1,4 @@
- import React, {useState} from 'react';
+import React, {useState} from 'react';
  import './ContactUs.css';
 
  export default function ContactUs() {
@@ -11,7 +11,7 @@
     });
     // handler to toggle form visibility
     const handleButtonClick = () => {
-        setIsFormVisible(true);
+        setIsFormVisible(!isFormVisible);
     };
     // handler to update form data
     const handleInputChange =(e) => {
@@ -21,6 +21,13 @@
             [name]: value
         }));
     }
+    // handler for submit button inside the form 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Thank you ${formData.name}. Your response has been recorded`);
+        setFormData({name: "", email: "", message: ""});
+        setIsFormVisible(false);
+    }
     return (
          <div className="contact-us-container">
             <h1 className='contact-us-header'>Contact Us</h1>
@@ -28,7 +35,50 @@
                 <p>Want more customers, Clients and Sales than you 
                     could handle? Reach out to us today!
                 </p>
-                <button className="contact-us-button">Get in Touch</button>
+                {!isFormVisible && (
+                <button className="contact-us-button" onClick={handleButtonClick}>Get in Touch</button>
+                )}
+                {isFormVisible &&(
+                    <div className='form-container'>
+                        <h2>Tell us about your business</h2>
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <label>
+                                    Username:
+                                    <input
+                                    type='text'
+                                    name='name'
+                                    value={formData.name}
+                                    onChange={handleInputChange} 
+                                    />
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    Email:
+                                    <input
+                                    type='email'
+                                    name='email'
+                                    value={formData.email}
+                                    onChange={handleInputChange} 
+                                    />                                    
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    Message:
+                                    <input
+                                    type='text'
+                                    name='message'
+                                    value={formData.message}
+                                    onChange={handleInputChange} 
+                                    />                                    
+                                </label>
+                            </div>
+                            <button type='submit'>Submit Data</button>
+                        </form>
+                    </div>
+                )}
             </div>
          </div>   
     );
